2517d9df046c529a8135b72bce5f10d945aca407
Crystal Studio <notwirachai@gmail.com>
2023-05-12 12:12:55 +0700

    fix(player): login

--- dx-backoffice/Controllers/Player.cs
+++ dx-backoffice/Controllers/Player.cs
@@ -9,6 +9,13 @@
     using System.Threading.Tasks;
     using System.Xml.Linq;
 
+    public class LoginInputModel
+    {
+        public string? CustomId { get; set; }
+
+        public bool? CreateAccount { get; set; }
+    }
+
     [ApiController]
     [Route("[controller]")]
     public class Player : ControllerBase
@@ -20,6 +27,7 @@
             string url = $"https://{titleId}.playfabapi.com/Inventory/GetInventoryCollectionIds";
 
             var data = new Dictionary<string, object> { { "Count", 1 }, };
+           
             string json = JsonSerializer.Serialize(data);
 
             var content = new StringContent(json, Encoding.UTF8, "application/json");
@@ -33,5 +41,32 @@
             Console.WriteLine("jsonResult =>> " + jsonResult);
             return jsonResult;
         }
+
+        [HttpPost]
+        public async Task<dynamic> LogInCustomIdAsync(HttpClient httpClient, [FromHeader(Name = "X-SecretKey")] string SecretKey, [FromBody] LoginInputModel loginInputModel )
+        {
+            string titleId = "2506C";
+            string url = $"https://{titleId}.playfabapi.com/Client/LoginWithCustomID";
+
+            var data = new Dictionary<string, object>  
+            {
+                { "TitleId", titleId },
+                { "CustomId", loginInputModel.CustomId },
+                { "CreateAccount", loginInputModel.CreateAccount },
+            };
+            string json = JsonSerializer.Serialize(data);
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            httpClient.DefaultRequestHeaders.Add("X-SecretKey", SecretKey);
+            HttpResponseMessage response = await httpClient.PostAsync(url, content);
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            dynamic jsonResult = JsonSerializer.Deserialize<dynamic>(responseBody);
+            Console.WriteLine("response =>> " + responseBody);
+            Console.WriteLine("jsonResult =>> " + jsonResult);
+            return jsonResult;
+        }
     }
+
 }
