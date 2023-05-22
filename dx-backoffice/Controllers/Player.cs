3c727e4d094e55032238b3bda8b901d6a51dd13c
Crystal Studio <notwirachai@gmail.com>
2023-05-22 14:28:44 +0700

    fix(player): Title Id

--- dx-backoffice/Controllers/Player.cs
+++ dx-backoffice/Controllers/Player.cs
@@ -16,10 +16,11 @@
     [Route("[controller]")]
     public class Player : ControllerBase
     {
+        string titleId = "2506C";
+
         [HttpPost("collectionId")]
         public async Task<dynamic> GetInventoryCollectionIdsAsync(HttpClient httpClient, [FromHeader(Name = "X-EntityToken")] string entityToken, [FromBody] CollectionInputModel collectionInputModel)
         {
-            string titleId = "2506C";
             string url = $"https://{titleId}.playfabapi.com/Inventory/GetInventoryCollectionIds";
 
             var data = new Dictionary<string, object>
@@ -76,99 +77,131 @@
             return new JsonResult(new { code = 400, status = "Bad Request", error = "Unable to retrieve collection data" });
         }
 
-        [HttpPost("Login")]
-        public async Task<dynamic> LogInCustomIdAsync(HttpClient httpClient, [FromHeader(Name = "X-SecretKey")] string SecretKey, [FromBody] LoginInputModel loginInputModel )
+        [HttpPost("NewCollectionId")]
+        public async Task<dynamic> LogInCustomIdAsync(HttpClient httpClient, [FromHeader(Name = "X-EntityToken")] string entityToken, [FromBody] CollectionInputModel collectionModel)
         {
-            string titleId = "2506C";
-            string url = $"https://{titleId}.playfabapi.com/Client/LoginWithCustomID";
+            string url = $"https://{titleId}.playfabapi.com/Inventory/GetInventoryCollectionIds";
 
             var data = new Dictionary<string, object>
-            {
-                { "TitleId", titleId },
-                { "CustomId", loginInputModel.CustomId },
-                { "CreateAccount", true },
-            };
-            string json = JsonSerializer.Serialize(data);
+    {
+        { "Count", 10 },
+    };
 
+            string json = JsonSerializer.Serialize(data);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            httpClient.DefaultRequestHeaders.Add("X-SecretKey", SecretKey);
+            httpClient.DefaultRequestHeaders.Add("X-EntityToken", entityToken);
+
             HttpResponseMessage response = await httpClient.PostAsync(url, content);
-
             string responseBody = await response.Content.ReadAsStringAsync();
-            JsonDocument jsonResult = JsonDocument.Parse(responseBody);
-
-            JsonElement statusProperty = default;
-            JsonElement dataProperty = default;
-            JsonElement newlyCreatedProperty = default;
-            JsonElement entityTokenProperty = default;
-
-            string latestCollectionId = string.Empty;
+            dynamic jsonResult = JsonSerializer.Deserialize<dynamic>(responseBody);
 
             StateModel stateModel = new StateModel();
-            PlayerDataService playerDataService = new PlayerDataService();
             UniqueIdModel uniqueIdModel = new UniqueIdModel();
-
+            PlayerDataService playerDataService = new PlayerDataService();
+            GetCurrentCollectionId getCurrentCollectionId = new GetCurrentCollectionId();
             uniqueIdModel.GenerateUniqueId();
 
             string randomState = stateModel.GetRandomState();
 
-            if (jsonResult.RootElement.TryGetProperty("status", out statusProperty) &&
-                statusProperty.GetString() == "OK")
-            {
-                if (jsonResult.RootElement.TryGetProperty("data", out dataProperty) &&
-                    dataProperty.TryGetProperty("NewlyCreated", out newlyCreatedProperty) &&
-                    newlyCreatedProperty.GetBoolean() &&
-                    dataProperty.TryGetProperty("EntityToken", out entityTokenProperty))
-                {
-                    string playFabId = jsonResult.RootElement.GetProperty("data").GetProperty("PlayFabId").GetString();
-                    string entityToken = jsonResult.RootElement.GetProperty("data").GetProperty("EntityToken").GetProperty("EntityToken").GetString();
-                    string entityId = jsonResult.RootElement.GetProperty("data").GetProperty("EntityToken").GetProperty("Entity").GetProperty("Id").GetString();
+            string latestCollectionId = string.Empty;
 
-                    var requestBody = new Dictionary<string, object>
+            if (jsonResult.ValueKind == JsonValueKind.Object)
+            {
+                if (jsonResult.TryGetProperty("data", out JsonElement dataElement) && dataElement.ValueKind == JsonValueKind.Object)
+                {
+                    if (dataElement.TryGetProperty("CollectionIds", out JsonElement collectionIdsElement) && collectionIdsElement.ValueKind == JsonValueKind.Array && collectionIdsElement.GetArrayLength() > 0)
+                    {
+                        // CollectionIds has values
+                        Console.WriteLine("CollectionIds has values");
+
+                        List<string> collectionIds = new List<string>();
+                        foreach (var collectionIdElement in collectionIdsElement.EnumerateArray())
+                        {
+                            string collectionId = collectionIdElement.GetString();
+                            collectionIds.Add(collectionId);
+                        }
+                        latestCollectionId = await getCurrentCollectionId.GetUserDataAsync(httpClient, collectionModel.PlayFabId) as string;
+
+                        var responseData = new
+                        {
+                            code = 200,
+                            status = "OK",
+                            data = new
+                            {
+                                CollectionIds = collectionIds,
+                                CurrentCollecitonId = latestCollectionId
+                            }
+                        };
+
+                        return new JsonResult(responseData);
+                    }
+                    else
+                    {
+                        // CollectionIds is empty
+                        Console.WriteLine("CollectionIds is empty");
+
+                        var requestBody = new Dictionary<string, object>
                     {
                         { "Amount", 1 },
                         { "Entity", new Dictionary<string, object>
                             {
-                                { "Id", entityId },
+                                { "Id", collectionModel.EntityId },
                                 { "Type", "title_player_account" },
                                 { "TypeString", "title_player_account" }
                             }
                         },
                         { "Item", new Dictionary<string, object>
                             {
-                                { "Id", "fbfe99f5-f593-4577-b9b4-865386073b30" },
+                                { "Id", "eb5e205a-06e0-456e-a7a8-3fc04a1976bf" },
                                 { "Type ", "bundle" },
                             }
                         },
                         {"CollectionId", randomState+"-"+uniqueIdModel.uniqueId }
                     };
 
-                    string collectionJson = JsonSerializer.Serialize(requestBody);
-                    var collectionContent = new StringContent(collectionJson, Encoding.UTF8, "application/json");
+                        string collectionJson = JsonSerializer.Serialize(requestBody);
+                        var collectionContent = new StringContent(collectionJson, Encoding.UTF8, "application/json");
 
-                    string urlCollectionIds = $"https://{titleId}.playfabapi.com/Inventory/AddInventoryItems";
-                    httpClient.DefaultRequestHeaders.Clear();
-                    httpClient.DefaultRequestHeaders.Add("X-EntityToken", entityToken);
-                    HttpResponseMessage responseCollectionIds = await httpClient.PostAsync(urlCollectionIds, collectionContent);
+                        string urlCollectionIds = $"https://{titleId}.playfabapi.com/Inventory/AddInventoryItems";
+                        HttpResponseMessage responseCollectionIds = await httpClient.PostAsync(urlCollectionIds, collectionContent);
 
-                    string responseBodyCollectionIds = await responseCollectionIds.Content.ReadAsStringAsync();
-                    dynamic jsonResultCollectionIds = JsonSerializer.Deserialize<dynamic>(responseBodyCollectionIds);
+                        string responseBodyCollectionIds = await responseCollectionIds.Content.ReadAsStringAsync();
+                        dynamic jsonResultCollectionIds = JsonSerializer.Deserialize<dynamic>(responseBodyCollectionIds);
 
-                    latestCollectionId = requestBody["CollectionId"].ToString();
+                        latestCollectionId = requestBody["CollectionId"].ToString();
 
-                    await playerDataService.UpdateUserDataAsync(httpClient, playFabId, latestCollectionId);
-
+                        await playerDataService.UpdateUserDataAsync(httpClient, collectionModel.PlayFabId, latestCollectionId);
+                        var newResponseData = new
+                        {
+                            code = 200,
+                            status = "OK",
+                            data = new
+                            {
+                                CurrentCollecitonId = latestCollectionId
+                            }
+                        };
+                        return new JsonResult(newResponseData);
+                        // return new JsonResult("CollectionIds is empty");
+                    }
+                }
+                else
+                {
+                    // CollectionIds is missing
+                    return new JsonResult("CollectionIds is missing");
                 }
             }
+            else
+            {
+                // Invalid response format
+                return new JsonResult("Invalid response format");
+            }
 
-            return jsonResult;
         }
 
         [HttpGet("state")]
         public async Task<dynamic> GetAllStateAsync(HttpClient httpClient)
         {
-            string titleId = "2506C";
             string url = $"https://{titleId}.playfabapi.com/Server/GetTitleData";
 
             var data = new Dictionary<string, object>
@@ -215,6 +248,63 @@
             string responseBody = await response.Content.ReadAsStringAsync();
 
             return responseBody;
+        }
+
+        [HttpPost("addCollectionId")]
+        public async Task<dynamic> AddCollectionId(HttpClient httpClient, [FromHeader(Name = "X-EntityToken")] string entityToken, [FromBody] CollectionInputModel collectionModel)
+        {
+            StateModel stateModel = new StateModel();
+            UniqueIdModel uniqueIdModel = new UniqueIdModel();
+            PlayerDataService playerDataService = new PlayerDataService();
+
+            uniqueIdModel.GenerateUniqueId();
+
+            string randomState = stateModel.GetRandomState();
+
+            string latestCollectionId = string.Empty;
+
+            var requestBody = new Dictionary<string, object>
+                    {
+                        { "Amount", 1 },
+                        { "Entity", new Dictionary<string, object>
+                            {
+                                { "Id", collectionModel.EntityId },
+                                { "Type", "title_player_account" },
+                                { "TypeString", "title_player_account" }
+                            }
+                        },
+                        { "Item", new Dictionary<string, object>
+                            {
+                                { "Id", "eb5e205a-06e0-456e-a7a8-3fc04a1976bf" },
+                                { "Type ", "bundle" },
+                            }
+                        },
+                        {"CollectionId", randomState+"-"+uniqueIdModel.uniqueId }
+                    };
+
+            string collectionJson = JsonSerializer.Serialize(requestBody);
+            var collectionContent = new StringContent(collectionJson, Encoding.UTF8, "application/json");
+
+            string urlCollectionIds = $"https://{titleId}.playfabapi.com/Inventory/AddInventoryItems";
+            httpClient.DefaultRequestHeaders.Add("X-EntityToken", entityToken);
+            HttpResponseMessage responseCollectionIds = await httpClient.PostAsync(urlCollectionIds, collectionContent);
+
+            string responseBodyCollectionIds = await responseCollectionIds.Content.ReadAsStringAsync();
+            dynamic jsonResultCollectionIds = JsonSerializer.Deserialize<dynamic>(responseBodyCollectionIds);
+
+            latestCollectionId = requestBody["CollectionId"].ToString();
+
+            await playerDataService.UpdateUserDataAsync(httpClient, collectionModel.PlayFabId, latestCollectionId);
+            var newResponseData = new
+            {
+                code = 200,
+                status = "OK",
+                data = new
+                {
+                    CurrentCollecitonId = latestCollectionId
+                }
+            };
+            return new JsonResult(newResponseData);
         }
     }
 
